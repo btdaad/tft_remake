d24854b315c81907c5069086762e9254d109b604
Axelle MANDY <axelle.mandy@epita.fr>
2025-09-25 17:02:24 +0200

    fix(board/dragndrop): units are now draggable with the mouse

--- TFT Remake/Assets/Scenes/Scripts/MouseManager/DragAndDrop.cs
+++ TFT Remake/Assets/Scenes/Scripts/MouseManager/DragAndDrop.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using System;
 
 public class DragAndDrop : MonoBehaviour
 {
+    [SerializeField] LayerMask mask;
+    [SerializeField] float dragHeight = 0.0f;
     Camera _camera;
-    
+    Transform _unitTransform;
+    float _unitHeight;
+    float _unitDistanceFromCamera;
+
     void Start()
     {
-        _camera = Camera.main;    
+        _camera = Camera.main;
+        _unitTransform = null;
+        _unitHeight = 0.0f;
+        _unitDistanceFromCamera = 0.0f;
     }
 
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 1000f;
-        mousePos = _camera.ScreenToWorldPoint(mousePos);
-        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);        
+        if (_unitTransform == null
+            && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                _unitTransform = hit.transform;
+                _unitHeight = _unitTransform.position.y;
+                _unitDistanceFromCamera = Vector3.Distance(_camera.transform.position, hit.point);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _unitTransform = null;
+            _unitHeight = 0.0f;
+            _unitDistanceFromCamera = 0.0f;
+        }
+        else if (_unitTransform != null)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = _unitDistanceFromCamera;
+            Vector3 unitPosition = _camera.ScreenToWorldPoint(mousePos);
+            unitPosition.y = Math.Max(unitPosition.y, _unitHeight);
+            unitPosition.y = Math.Min(unitPosition.y, _unitHeight + dragHeight);
+            _unitTransform.position = unitPosition;
+        }
     }
 }
