4ac4e312f063bab97e4771f76d0250da93e24710
Axelle MANDY <axelle.mandy@epita.fr>
2025-09-26 15:05:45 +0200

    save(board/manager): dump positions

--- TFT Remake/Assets/Scenes/Scripts/Board/BoardManager.cs
+++ TFT Remake/Assets/Scenes/Scripts/Board/BoardManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System;
 
 public class BoardManager : MonoBehaviour
 {
@@ -7,8 +8,8 @@ public class BoardManager : MonoBehaviour
     Tilemap _playerBattlefield;
     Tilemap _playerBench;
 
-    Transform[,] _battlefield;
-    Transform[,] _bench;
+    Transform[][] _battlefield;
+    Transform[][] _bench;
 
     void Awake()
     {
@@ -30,12 +31,19 @@ public class BoardManager : MonoBehaviour
 
         BoundsInt battlefieldBounds = _playerBattlefield.cellBounds;
         Vector3Int battlefieldSize = battlefieldBounds.size + battlefieldBounds.position;
-        _battlefield = new Transform[battlefieldSize.x, battlefieldSize.y];
+        _battlefield = new Transform[battlefieldSize.y][];
+        for (int i = 0; i < battlefieldSize.y; i++)
+            _battlefield[i] = new Transform[battlefieldSize.x];
+        Debug.Log("_battlefield[" + battlefieldSize.y + "][" + battlefieldSize.x + "]");
+        DumpBoard(_battlefield);
 
         BoundsInt benchBounds = _playerBench.cellBounds;
         Vector3Int benchSize = benchBounds.size + benchBounds.position;
-        _bench = new Transform[benchSize.x, 2];
-        
+        _bench = new Transform[2][];
+        for (int i = 0; i < 2; i++)
+            _bench[i] = new Transform[benchSize.x];
+        Debug.Log("_bench[" + 2 + "][" + benchSize.x + "]");
+        DumpBoard(_bench);
     }
 
     public void OnDragUnit(Transform unitTransform)
@@ -59,15 +67,38 @@ public class BoardManager : MonoBehaviour
     private bool DropOnZone(Transform unitTransform, Vector3 unitPos, Tilemap zone)
     {
         Vector3Int cellPos = zone.WorldToCell(unitPos);
-        Debug.Log("Cell Bounds: " + zone.cellBounds);
 
         if (zone.cellBounds.Contains(cellPos) && zone.HasTile(cellPos))
         {
-            Debug.Log("Cell Pos: " + cellPos);
             Vector3 cellCenterPos = zone.GetCellCenterWorld(cellPos);
             unitTransform.position = new Vector3(cellCenterPos.x, _initUnitPos.y, cellCenterPos.z);
+            PlaceOnBoard(unitTransform, cellPos, zone);
             return true;
         }
         return false;
+    }
+
+    private void PlaceOnBoard(Transform unitTransform, Vector3Int cellPos, Tilemap zone)
+    {
+        if (zone == _playerBattlefield)
+            _battlefield[cellPos.y + 1][cellPos.x + 1] = unitTransform;
+        else
+            _bench[cellPos.y == -1 ? 0 : 1][cellPos.x + 1] = unitTransform;
+
+        DumpBoard(_battlefield);
+        DumpBoard(_bench);
+    }
+
+    private void DumpBoard(Transform[][] board)
+    {
+        string str = "[";
+        foreach (var row in board)
+        {
+            str += "[";
+            foreach (var cell in row)
+                str += cell + ", ";
+            str += "]\n";
+        }
+        Debug.Log(str + "]");
     }
 }
