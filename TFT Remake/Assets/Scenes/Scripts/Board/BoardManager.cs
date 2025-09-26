82eb235165fc3973b905264a2d5a49ce27c15ec2
Axelle MANDY <axelle.mandy@epita.fr>
2025-09-26 11:04:21 +0200

    fix(board/manager): create multidimensional arrays to manage unit positionning

--- TFT Remake/Assets/Scenes/Scripts/Board/BoardManager.cs
+++ TFT Remake/Assets/Scenes/Scripts/Board/BoardManager.cs
@@ -7,6 +7,9 @@ public class BoardManager : MonoBehaviour
     Tilemap _playerBattlefield;
     Tilemap _playerBench;
 
+    Transform[,] _battlefield;
+    Transform[,] _bench;
+
     void Awake()
     {
         _playerBattlefield = null;
@@ -24,6 +27,15 @@ public class BoardManager : MonoBehaviour
             else if (tilemap.CompareTag("Player Bench"))
                 _playerBench = tilemap;
         }
+
+        BoundsInt battlefieldBounds = _playerBattlefield.cellBounds;
+        Vector3Int battlefieldSize = battlefieldBounds.size + battlefieldBounds.position;
+        _battlefield = new Transform[battlefieldSize.x, battlefieldSize.y];
+
+        BoundsInt benchBounds = _playerBench.cellBounds;
+        Vector3Int benchSize = benchBounds.size + benchBounds.position;
+        _bench = new Transform[benchSize.x, 2];
+        
     }
 
     public void OnDragUnit(Transform unitTransform)
@@ -47,9 +59,11 @@ public class BoardManager : MonoBehaviour
     private bool DropOnZone(Transform unitTransform, Vector3 unitPos, Tilemap zone)
     {
         Vector3Int cellPos = zone.WorldToCell(unitPos);
+        Debug.Log("Cell Bounds: " + zone.cellBounds);
 
         if (zone.cellBounds.Contains(cellPos) && zone.HasTile(cellPos))
         {
+            Debug.Log("Cell Pos: " + cellPos);
             Vector3 cellCenterPos = zone.GetCellCenterWorld(cellPos);
             unitTransform.position = new Vector3(cellCenterPos.x, _initUnitPos.y, cellCenterPos.z);
             return true;
