7b8bc05be643349e6c076a44044c7b8f7a6118df
Axelle MANDY <axelle.mandy@epita.fr>
2025-09-30 11:56:03 +0200

    :sparkles: feat(unit/stats): create scriptable objects to hold stats info

---
+++ TFT Remake/Assets/Scenes/Scripts/UnitStats/UnitStats.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitStats", menuName = "Scriptable Objects/UnitStats")]
+public class UnitStats : ScriptableObject
+{
+    public enum Cost
+    {
+        OneCost = 1,
+        TwoCost = 2,
+        ThreeCost = 3,
+        // FourCost = 4,
+        // FiveCost = 5,
+        None = 0
+    };
+
+    public enum Star
+    {
+        OneStar = 0,
+        TwoStar = 1,
+        ThreeStar = 2,
+        None = -1
+    };
+    
+    public Cost _cost;
+    public Star _star;
+
+    [Header("Stats")]
+    public float[] health = new float[3];
+    public float[] mana = new float[3];
+    public float[] attackDamage = new float[3];
+    public float abilityPower;
+    public float armor;
+    public float magicResist;
+    public float attackSpeed;
+    public float critChance;
+    public float critDamage;
+    public int range;
+}
