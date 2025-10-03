4b80bda841257e47cf1ee23dbcabe42a2c2c47c1
Axelle MANDY <axelle.mandy@epita.fr>
2025-10-03 15:59:06 +0200

    :construction: save(ui/stats): use basic number display, changing to logo and reorganizing stat display

--- TFT Remake/Assets/Scripts/UIManager/UIManager.cs
+++ TFT Remake/Assets/Scripts/UIManager/UIManager.cs
@@ -1,37 +1,39 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEditor;
 
 public class UIManager : MonoBehaviour
 {
-    public UIDocument UIDoc;
+    [SerializeField] UIDocument UIDoc;
+    private VisualElement _unitDisplayBackground;
     private Label _healthLabel;
     private VisualElement _healthBarMask;
-    private Canvas _unitDisplay = null;
-    private RawImage _unitInfo = null;
-    private RawImage _unitArt = null;
+    private Label _manaLabel;
+    private VisualElement _manaBarMask;
+    private Label _name;
+    private Label _ap;
+    private Label _ad;
+    private Label _armor;
+    private Label _rm;
+    private Label _atkSpeed;
+    private Label _crit;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Init()
     {
-        _unitDisplay = (Canvas)transform.Find("Unit Display").GetComponent<Canvas>();
-        if (_unitDisplay == null)
-        {
-            Debug.LogError("Could not find a Unit Display canvas.");
-            return;
-        }
-
-        _unitInfo = (RawImage)_unitDisplay.transform.Find("Unit Info").GetComponent<RawImage>();
-        if (_unitInfo == null)
-            Debug.LogError("Could not find Unit Info raw image.");
-        _unitArt = (RawImage)_unitDisplay.transform.Find("Unit Art").GetComponent<RawImage>();
-        if (_unitArt == null)
-            Debug.LogError("Could not find Unit Art raw image.");
-
-        _unitDisplay.gameObject.SetActive(false);
-        
+        _unitDisplayBackground = UIDoc.rootVisualElement.Q<VisualElement>("UnitDisplayBackground");
         _healthLabel = UIDoc.rootVisualElement.Q<Label>("HealthLabel");
         _healthBarMask = UIDoc.rootVisualElement.Q<VisualElement>("HealthBarMask");
-        _healthLabel.text = $"{500}/{500}";
+        _manaLabel = UIDoc.rootVisualElement.Q<Label>("ManaLabel");
+        _manaBarMask = UIDoc.rootVisualElement.Q<VisualElement>("ManaBarMask");
+        _name = UIDoc.rootVisualElement.Q<Label>("Name");
+        _ap = UIDoc.rootVisualElement.Q<Label>("AP");
+        _ad = UIDoc.rootVisualElement.Q<Label>("AD");
+        _armor = UIDoc.rootVisualElement.Q<Label>("Armor");
+        _rm = UIDoc.rootVisualElement.Q<Label>("RM");
+        _atkSpeed = UIDoc.rootVisualElement.Q<Label>("AtkSpeed");
+        _crit = UIDoc.rootVisualElement.Q<Label>("Crit");
+
+        _unitDisplayBackground.visible = false;
     }
 
     // Update is called once per frame
@@ -39,14 +41,29 @@ public class UIManager : MonoBehaviour
     {
     }
 
-    public void ShowUnitDisplay(Transform unit)
+    public void ShowUnitDisplay(Transform unitTransform)
     {
-        _unitDisplay.gameObject.SetActive(true);
-        _unitArt.material = unit.GetComponent<Renderer>().material;
+        Unit unit = unitTransform.GetComponent<Unit>(); 
+        UnitStats stats = unit.stats;
+
+        _healthLabel.text = $"{unit.GetHealth()}/{stats.health[(int) stats.star]}";
+        _manaLabel.text = $"{unit.GetMana()}/{stats.mana[(int) stats.star]}";
+
+        _name.text = unit.name;
+
+        _ap.text = $"{stats.abilityPower}";
+        _ad.text = $"{stats.attackDamage[(int)stats.star]}";
+        _armor.text = $"{stats.armor}";
+        _rm.text = $"{stats.magicResist}";
+        _atkSpeed.text = $"{stats.attackSpeed}";
+        _crit.text = $"{stats.critChance}%";
+
+        // _unitArt.material = unit.GetComponent<Renderer>().material;
+        _unitDisplayBackground.visible = true;
     }
 
     public void HideUnitDisplay()
     {
-        _unitDisplay.gameObject.SetActive(false);
+        _unitDisplayBackground.visible = false;
     }
 }
--- TFT Remake/Assets/Scripts/Unit/Unit.cs
+++ TFT Remake/Assets/Scripts/Unit/Unit.cs
@@ -3,15 +3,28 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] public UnitStats stats;
+    float _health;
+    float _mana;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _health = stats.health[(int)stats.star];
+        _mana = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public float GetHealth()
+    {
+        return _health;
+    }
+
+    public float GetMana()
+    {
+        return _mana;
     }
 }
